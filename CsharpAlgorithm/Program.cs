9617cea35e99b8cca4a06a18229371436f410d48
taejunHwang <taejun.dev@gmail.com>
2020-09-05 19:33:36 +0900

    using 추가 및 picnic 동적할당

--- CsharpAlgorithm/Program.cs
+++ CsharpAlgorithm/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CsharpAlgorithm.Source.Algospot;
 
 namespace CsharpAlgorithm
 {
@@ -10,6 +11,7 @@ namespace CsharpAlgorithm
     {
         static void Main(string[] args)
         {
+            Picnic picnic = new Picnic();
         }
     }
 }
