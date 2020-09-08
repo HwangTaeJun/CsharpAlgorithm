5b63d0851c153f4c92c03657abee21ec6ff0207a
taejunHwang <taejun.dev@gmail.com>
2020-09-08 12:55:46 +0900

    폴더 추가 및 조합 알고리즘 클래스 추가

--- CsharpAlgorithm/Program.cs
+++ CsharpAlgorithm/Program.cs
@@ -9,6 +9,7 @@ using CsharpAlgorithm.Source.BackJoon;
 using CsharpAlgorithm.Source.NYPC._2019;
 using CsharpAlgorithm.Source.Programmers.FullSearch;
 using CsharpAlgorithm.Source.Programmers.DynamicProgramming;
+using CsharpAlgorithm.Source.Others;
 
 namespace CsharpAlgorithm
 {
@@ -20,8 +21,29 @@ namespace CsharpAlgorithm
 
             Picnic picnic = new Picnic();
             SugarDelivery sugarDelivery = new SugarDelivery();
-            MabinogiInventory mabinogiInventory = new MabinogiInventory();
+            //MabinogiInventory mabinogiInventory = new MabinogiInventory();
             MockTest mockTest = new MockTest();
+
+            int n = 3;
+            int cnt = 0;
+            Permutation perm = new Permutation(n);
+            List<string> number = new List<string>();
+
+            while (true)
+            {
+                int[] c = perm.next();
+
+                string str = null;
+
+                if (c == null) break;
+                for (int i = 0; i < c.Length; i++)
+                {
+                    str += c[i].ToString();
+                }
+
+                number.Add(str);
+            }
+
             FindPrimeNumber findPrimeNumber = new FindPrimeNumber();
         }
     }
---
+++ CsharpAlgorithm/Source/Others/Combination.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAlgorithm.Source.Others
+{
+    class Combination
+    {
+        private int[] result;
+        private Stack<int> stack;
+        private int r, n;
+
+
+        public Combination(int r, int n)
+        {
+            Console.WriteLine("\nCombination\n");
+            // nCr 계산
+            this.r = r;
+            this.n = n;
+
+            result = new int[r];
+            stack = new Stack<int>();
+            stack.Push(0);
+        }
+
+        public int[] next()
+        {   
+            while (stack.Count > 0)
+            {
+                int index = stack.Count - 1;
+                int value = stack.Pop();
+
+                while (value < n)
+                {
+                    result[index++] = value++;
+                    stack.Push(value);
+                    if (index == r)
+                    {
+                        return result;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
