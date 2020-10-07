27f5425b7debb8e20dbe9ffa8c63b894b05968a7
황태준 <taejun.dev@gmail.com>
2020-10-07 22:44:27 +0900

    단어 뒤집기 풀이
    폴더 및 클래스 생성

---
+++ CsharpAlgorithm/Source/Others/String/ReverseWord.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAlgorithm.Source
+{
+    /// <summary>
+    /// 단어 뒤집기 (프로그래밍 면접 이렇게 준비한다. p211)
+    /// </summary>
+    class ReverseWord
+    {
+        public ReverseWord()
+        {
+            string exampleStr = "Do or do not, there is no try.";
+
+            Console.WriteLine(Solution(exampleStr));
+        }
+
+        public string Solution(string str)
+        {
+            Stack<StringBuilder> reverseWordSBStack = new Stack<StringBuilder>();
+            reverseWordSBStack.Push(new StringBuilder(str.Length, str.Length));
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                reverseWordSBStack.Peek().Append(str[i]);
+
+                if (str[i] == ' ')
+                {
+                    reverseWordSBStack.Push(new StringBuilder(str.Length, str.Length));
+                }
+            }
+
+            StringBuilder resultSB = new StringBuilder(str.Length, str.Length);
+
+            foreach (var item in reverseWordSBStack)
+            {
+                resultSB.Append(item);
+            }
+
+            return resultSB.ToString();
+        }
+    }
+}
