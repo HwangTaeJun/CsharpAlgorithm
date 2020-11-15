248e7b00afc7125bccc69603696098f6a7d1e599
황태준 <taejun.dev@gmail.com>
2020-11-15 18:51:40 +0900

    MyStack 클래스 생성 및 구현
    Push,Pop,Peek,IsFull,IsEmpty 구현

--- CsharpAlgorithm/Program.cs
+++ CsharpAlgorithm/Program.cs
@@ -4,55 +4,27 @@ using System.Text;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using CsharpAlgorithm.Source;
+using CsharpAlgorithm.Source.DataStructure;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        MyDoubleLinkedList<int> myDoubleLinkedList = new MyDoubleLinkedList<int>();
-        MySingleLinkedList<int> mySingleLinkedList = new MySingleLinkedList<int>();
-        MyQueue<int> myQueue = new MyQueue<int>();
-        Queue<int> test = new Queue<int>();
-        Stack<int> tests = new Stack<int>();
+        MyStack<int> myStack = new MyStack<int>();
 
         for (int i = 0; i < 5; i++)
         {
-            myQueue.Enqueue(i);
+            myStack.Push(i);
         }
 
-        Console.WriteLine(myQueue.Peek());
+        Console.WriteLine(myStack.Pop());
+        Console.WriteLine(myStack.Peek());
+        Console.WriteLine(myStack.Pop());
+        Console.WriteLine(myStack.Peek());
 
-        Console.WriteLine(myQueue.Dequeue());
-        Console.WriteLine(myQueue.Dequeue());
-
-        myQueue.Enqueue(5);
-        myQueue.Enqueue(6);
-
-        Console.WriteLine(myQueue.Peek());
-
-        for (int i = 1; i < 6; i++)
+        for (int i = 0; i < 5; i++)
         {
-            myQueue.Enqueue(i * 10);
-        }
-
-        for (int i = 0; i < 3; i++)
-        {
-            Console.WriteLine(myQueue.Dequeue());
-        }
-
-        for (int i = 1; i < 10; i++)
-        {
-            myQueue.Enqueue(i * 100);
-        }
-
-        for (int i = 0; i < 10; i++)
-        {
-            myQueue.Dequeue();
-        }
-
-        for (int i = 0; i < 10; i++)
-        {
-            myQueue.Enqueue((i+1) * 1000);
+            myStack.Push((i + 1)*10);
         }
 
         System.Console.ReadLine();
---
+++ CsharpAlgorithm/Source/DataStructure/MyStack.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAlgorithm.Source.DataStructure
+{
+    class MyStack<T>
+    {
+        private T[] dataArr = null;
+
+        private int index = -1;
+        private int capacity = 5;
+
+        public int Count { get; private set; } = 0;
+
+        public MyStack()
+        {
+            dataArr = new T[capacity];
+        }
+
+        public void Push(T data)
+        {
+            if(IsFull())
+            {
+                Resize();
+            }
+
+            index += 1;
+
+            dataArr[index] = data;
+
+            Count++;
+        }
+
+
+        public T Pop()
+        {
+            T data = dataArr[index];
+
+            index--;
+            Count--;
+
+            return data;
+        }
+
+        public T Peek()
+        {
+            return dataArr[index];
+        }
+
+        public bool IsFull()
+        {
+            if(Count >= capacity)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool IsEmpty()
+        {
+            if(Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Resize()
+        {
+            capacity *= 2;
+
+            T[] newDataArr = new T[capacity];
+
+            dataArr.CopyTo(newDataArr, 0);
+
+            dataArr = newDataArr;
+        }
+    }
+}
