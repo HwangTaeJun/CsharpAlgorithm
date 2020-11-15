ea86b7d00f8f9cf6837dc61e70aea6009dc5ed60
황태준 <taejun.dev@gmail.com>
2020-11-15 15:37:46 +0900

    MyQueue 클래스 생성 및 구현
    Circular Queue로 구현
    Enqeueu, Deqeueu, Peek, IsEmpty, IsFull, Resize 함수 구현

--- CsharpAlgorithm/Program.cs
+++ CsharpAlgorithm/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
@@ -12,11 +11,20 @@ public class Program
     {
         MyDoubleLinkedList<int> myDoubleLinkedList = new MyDoubleLinkedList<int>();
         MySingleLinkedList<int> mySingleLinkedList = new MySingleLinkedList<int>();
+        MyQueue<int> myQueue = new MyQueue<int>();
+        Queue<int> test = new Queue<int>();
 
         for (int i = 0; i < 5; i++)
         {
+            myQueue.Enqueue(i);
             myDoubleLinkedList.AddLast(i);
             mySingleLinkedList.AddLast(i);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(myQueue.Peek() + "count : " +myQueue.Count);
+            myQueue.Dequeue();
         }
 
         myDoubleLinkedList.Reverse();
---
+++ CsharpAlgorithm/Source/DataStructure/MyQueue.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpAlgorithm.Source
+{
+    class MyQueue<T>
+    {
+        private T[] dataArr = null;
+
+        private T frontData;
+
+        private int front = -1;
+        private int rear = -1;
+
+        private int capacity = 3;
+
+        public int Count { get; private set; } = 0;
+
+        public MyQueue()
+        {
+            dataArr = new T[capacity];
+        }
+
+        public void Enqueue(T data)
+        {
+            if(IsFull())
+            {
+                Resize();
+            }
+
+            if(front == rear)
+            {
+                frontData = data;
+            }
+
+            rear = (rear + 1) % capacity;
+            Count++;
+
+            dataArr[rear] = data;
+        }
+
+        public T Dequeue()
+        {
+            if(IsEmpty())
+            {
+                throw new Exception("Empty");
+            }
+
+            front = (front + 1) % capacity;
+            Count--;
+
+            if (!IsEmpty())
+            {
+                frontData = dataArr[front + 1];
+            }
+
+            return dataArr[front];
+        }
+
+        public T Peek()
+        {
+            if(IsEmpty())
+            {
+                throw new Exception("Empty");
+            }
+
+            return frontData;
+        }
+
+        private bool IsFull()
+        {
+            if(front == rear + 1 || capacity <= Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsEmpty()
+        {
+            if(rear == front)
+            {
+                frontData = default(T);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Resize()
+        {
+            capacity *= 2;
+
+            T[] newDataArr = new T[capacity];
+
+            dataArr.CopyTo(newDataArr, 0);
+            dataArr = newDataArr;
+        }
+    }
+}
