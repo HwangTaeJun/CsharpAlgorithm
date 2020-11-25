522fb9646ec6ba738ff6d167a90e0ad452931352
황태준 <taejun.dev@gmail.com>
2020-11-25 19:10:45 +0900

    Programmers 여행경로(DFS/BFS) 문제 풀이
    TravelRoute 클래스 생성 및 문제 링크 summary로 추가
    생성자 구현 - 문제 input에 따른 결과값이 일치 하는지 확인 하는 과정
    Solution 함수는 선언만 아직 미구현

--- CsharpAlgorithm/Program.cs
+++ CsharpAlgorithm/Program.cs
@@ -19,56 +19,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        MyPriorityQueue<PriorityData> myPriorityQueue = new MyPriorityQueue<PriorityData>();
-        PriorityQueue<PriorityData> priorityQueue = new PriorityQueue<PriorityData>();
-        Random random = new Random();
-        Stopwatch stopwatch = new Stopwatch();
-
-        string[] dataArr = new string[100000];
-        int[] priorityArr = new int[100000];
-
-        for (int i = 0; i < priorityArr.Length; i++)
-        {
-            int priority = random.Next(0, priorityArr.Length * 2);
-
-            priorityArr[i] = priority;
-            dataArr[i] = i.ToString();
-        }
-
-        for (int i = 0; i < priorityArr.Length; i++)
-        {
-            myPriorityQueue.Enqueue(new PriorityData(dataArr[i], priorityArr[i]));
-            priorityQueue.Enqueue(new PriorityData(dataArr[i], priorityArr[i]));
-
-            var myData = myPriorityQueue.Peek();
-            var data = priorityQueue.Peek();
-
-            if (myData.data != data.data)
-            {
-                Console.WriteLine("data가 다르다" + myData.data + ", " + data.data);
-            }
-
-            if (myData.priority != data.priority)
-            {
-                Console.WriteLine("priority 다르다" + myData.priority + ", " + data.priority);
-            }
-        }
-
-        for (int i = 0; i < priorityArr.Length; i++)
-        {
-            var myData = myPriorityQueue.Dequeue();
-            var data = priorityQueue.Dequeue();
-
-            if (myData.data != data.data)
-            {
-                Console.WriteLine(" Dequeue data가 다르다" + myData.data + ", " + data.data);
-            }
-
-            if (myData.priority != data.priority)
-            {
-                Console.WriteLine("Dequeue priority 다르다" + myData.priority + ", " + data.priority);
-            }
-        }
+        TravelRoute travelRoute = new TravelRoute();
 
         Console.ReadLine();
     }
---
+++ CsharpAlgorithm/Source/Algorithm/Programmers/DFS_BFS/TravelRoute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CsharpAlgorithm.Source
+{
+    /// <summary>
+    /// 여행경로 https://programmers.co.kr/learn/courses/30/lessons/43164
+    /// </summary>
+    class TravelRoute
+    {
+        //알고리즘 문제에서 제공되는 input에 따른 결과값이 일치하는지 비교
+        public TravelRoute()
+        {
+            List<string[,]> exampleInputList = new List<string[,]>();
+            List<string[]> exampleOutputList = new List<string[]>();
+
+            exampleInputList.Add(new string[,] { { "ICN", "JFK" }, { "HND", "IAD" }, { "JFK", "HND" } });
+            exampleInputList.Add(new string[,] { { "ICN", "SFO" }, { "ICN", "ATL" }, { "SFO", "ATL" }, { "ATL", "ICN" }, { "ATL", "SFO" } });
+
+            exampleOutputList.Add(new string[] { "ICN", "JFK", "HND", "IAD" });
+            exampleOutputList.Add(new string[] { "ICN", "ATL", "ICN", "SFO", "ATL", "SFO" });
+
+            for (int i = 0; i < exampleInputList.Count; i++)
+            {
+                string[] exampleOutput = exampleOutputList[i];
+                string[] myOutput = Solution(exampleInputList[i]);
+
+                Console.WriteLine(i + "번째 결과 " + exampleOutput.SequenceEqual(myOutput));
+            }
+        }
+
+        public string[] Solution(string[,] ticketArr)
+        {
+            return null;
+        }
+    }
+}
