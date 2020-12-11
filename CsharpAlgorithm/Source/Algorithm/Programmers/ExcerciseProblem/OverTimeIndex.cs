d449b2cb3a5f8018eab2db82b3969b292874b172
taejunHwang <taejun.dev@gmail.com>
2020-12-12 07:25:47 +0900

    프로그래머스 야근지수 풀이 완료
    work 처리 방식
    1. maxWorkAmount 값이 유일하게 가장 큰 값이면
    두번째로 큰 값의 차이 만큼 work를 처리하고
    
    2. 아니라면 두번째 값로 큰 값이 나오거나 마지막 index까지 접근 후
    index까지 work를 처리함

--- CsharpAlgorithm/Source/Algorithm/Programmers/ExcerciseProblem/OverTimeIndex.cs
+++ CsharpAlgorithm/Source/Algorithm/Programmers/ExcerciseProblem/OverTimeIndex.cs
@@ -11,8 +11,6 @@ namespace CsharpAlgorithm.Source
     /// </summary>
     class OverTimeIndex
     {
-        private List<int> workList = null;
-
         public OverTimeIndex()
         {
             List<int> inputOverTimeCountList = new List<int>() { 4, 1, 3 };
@@ -25,13 +23,77 @@ namespace CsharpAlgorithm.Source
 
             for (int i = 0; i < inputOverTimeCountList.Count; i++)
             {
-                Console.WriteLine(Solution(inputOverTimeCountList[i], inputWorkArrList[i]));
+                if (resultList[i] == Solution(inputOverTimeCountList[i], inputWorkArrList[i]))
+                {
+                    Console.WriteLine("입력에 따른 결과값이 일치함");
+                }
             }
         }
 
-        private int Solution(int overTimeCount, int[] workArr)
+        private long Solution(int overTimeCount, int[] workArr)
         {
-            int result = 0;
+            long result = 0;
+
+            Array.Sort(workArr);
+            Array.Reverse(workArr);
+
+            int maxWorkAmount = workArr[0];
+            int maxIndex = 0;
+            int secondMaxIndex = 0;
+
+            while (overTimeCount > 0)
+            {
+                if (workArr[workArr.Length - 1] <= 0)
+                {
+                    return 0;
+                }
+
+                if (maxWorkAmount > workArr[maxIndex + 1])
+                {
+                    int diff = maxWorkAmount - workArr[maxIndex + 1];
+
+                    if (diff > overTimeCount)
+                    {
+                        diff = overTimeCount;
+                    }
+
+                    overTimeCount -= diff;
+                    workArr[maxIndex] -= diff;
+                    maxWorkAmount -= diff;
+                }
+                else
+                {
+                    secondMaxIndex = workArr.Length;
+
+                    for (int i = maxIndex; i < workArr.Length; i++)
+                    {
+                        if (maxWorkAmount > workArr[i])
+                        {
+                            secondMaxIndex = i;
+                            break;
+                        }
+                    }
+
+                    for (int i = maxIndex; i < secondMaxIndex; i++)
+                    {
+                        overTimeCount--;
+                        workArr[i]--;
+
+                        if (overTimeCount == 0)
+                        {
+                            break;
+                        }
+                    }
+
+                    maxWorkAmount--;
+                }
+            }
+
+            for (int i = 0; i < workArr.Length; i++)
+            {
+                int num = workArr[i];
+                result += num * num;
+            }
 
             return result;
         }
