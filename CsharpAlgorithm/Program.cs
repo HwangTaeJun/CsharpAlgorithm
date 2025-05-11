f3922e8352778f7de75e0b719661c94f1a048242
황태준 <taejun@rp5entertainment.com>
2025-05-11 21:58:12 +0900

    프로그래머스 봉인된 주문

--- CsharpAlgorithm/Program.cs
+++ CsharpAlgorithm/Program.cs
@@ -5,20 +5,16 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using CsharpAlgorithm.Source;
 using CsharpAlgorithm.Source.DataStructure;
+using CsharpAlgorithm.Source.Algorithm.Programmers.Challenge;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        int[] dataArr = new int[] { 5, 2, 1, 0, 4, 55, 234, 12, 35, 23, 6, 7, 4, 434, 764 };
+        SealedSpell shortestMap = new SealedSpell();
 
-        MySort mySort = new MySort();
-
-        mySort.QuickSort(dataArr);
-        mySort.InsertSort(dataArr);
-        mySort.BubbleSort(dataArr);
-        mySort.SelectSort(dataArr);
-
+        //shortestMap.Result(30, new string[] { "d", "e", "bb", "aa", "ae" } );
+        shortestMap.solution(7388,new string[] { "gqk", "kdn", "jxj", "jxi", "fug", "jxg", "ewq", "len", "bhc" } );
         Console.ReadLine();
     }
 }
---
+++ CsharpAlgorithm/Source/Algorithm/Programmers/Challenge/SealedSpell.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace CsharpAlgorithm.Source.Algorithm.Programmers.Challenge
+{
+    //봉인된 주문
+    //https://school.programmers.co.kr/learn/courses/30/lessons/389481
+
+    class SealedSpell
+    {
+        public string Solution(long n, string[] bans)
+        {
+            var list = bans.Select(x => CalcBanNumber(x)).OrderBy(x=>x).ToList();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] <= n)
+                    n++;
+            }
+
+            var spell = CalcSpell(n);
+            return spell;
+        }
+
+        private long CalcBanNumber(string ban)
+        {
+            long value = 0;
+
+            foreach (char c in ban)
+            {
+                long numberValue = c - 'a' + 1;
+                value = value * 26 + numberValue;
+            }
+
+            return value;
+        }
+
+        private string CalcSpell(long value)
+        {
+            var sb = new StringBuilder();
+
+            while (value > 0)
+            {
+                value--;
+                char charValue = (char)('a' + (value % 26));
+                sb.Insert(0, charValue);
+                value /= 26;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
