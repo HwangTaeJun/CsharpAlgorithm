a393e7af36f0a8a399d010994eb9144292ca8d46
황태준 <taejun@rp5entertainment.com>
2025-05-14 02:48:17 +0900

    프로그래머스 - 서버 증설 횟수

---
+++ CsharpAlgorithm/Source/Algorithm/Programmers/Challenge/ServerExpansion.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CsharpAlgorithm.Source.Algorithm.Programmers.Challenge
+{
+    public class ServerExpansion
+    {
+        public int Solution(int[] players, int m, int k)
+        {
+            int answer = 0;
+            answer = CheckServer(players, m, k);
+            return answer;
+        }
+
+        private int CheckServer(int[] players, int m, int k)
+        {
+            var length = players.Length;
+            var result = 0;
+
+            Queue<int> serverTimer = new Queue<int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                RemoveServer(serverTimer);
+
+                var player = players[i];
+                var needCount = (int)(player / m);
+
+                if (needCount > 0 && (serverTimer.Count < needCount))
+                {
+                    var addCount = needCount - serverTimer.Count;
+
+                    for (int j = 0; j < addCount; j++)
+                    {
+                        serverTimer.Enqueue(k);
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private void RemoveServer(Queue<int> serverTimer)
+        {
+            if (serverTimer.Count == 0)
+                return;
+
+            var count = serverTimer.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                var timer = serverTimer.Dequeue();
+                timer--;
+
+                if (timer > 0)
+                    serverTimer.Enqueue(timer);
+            }
+        }
+    }
+}
